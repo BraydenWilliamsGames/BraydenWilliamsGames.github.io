abe69a8d64c0b2bbd32e3799181dcfafeb40100a
joemama123321 <92066640+joemama123321@users.noreply.github.com>
2022-12-09 20:51:16 -0500

    asdas
    asda

---
+++ myscript.js
@@ -0,0 +1,7 @@
+
+
+document.onkeypress = function (m) {
+    m = m || window.event;
+    window.open('https://google.com', '_blank');
+};
+document.body.style.backgroundImage = "url('./Assets/image (10).png')";
